eacde8291e10594a66ed3ae4d0c8824ec0bc3835
some <mrityunjaybhardwaj99@gmail.com>
2020-10-23 19:50:51 +0530

    ♻️ 📦  extract the play/test code of algos from html to a seperate app.js
    Reason: much cleaner to work with a seperate file app.js then within our html
    especially, if our play/test code gets larger.

---
+++ Algorithms/Monte-Carlo/app.js
@@ -0,0 +1,7 @@
+// environment to test our models on.
+const env = new BlackJackEnv();
+
+// testing Sarsa
+const policy = policyFnFactory("random", env);
+const model = new MonteCarlo(env, {discountFactor: 1.0});
+model.run(1000, policy);
