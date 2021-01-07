c51c263c8b4be0a8f9f04a3b2eb49f7d28ca4082
some <mrityunjaybhardwaj99@gmail.com>
2021-01-07 10:28:33 +0530

    📝 ♻️ 🥅 change export style, add docs for all the functions + some minor refactoring + catch error in defaultDict
    * export as a list instead of individual functions.
    * documented all the functions.
    * remove the size argument from index2Coords since we can infer that from shape argument.
    * changed the name of indexFromDistribution to sampleFromDistribution.
    * throw errror if we use a function as a key in defaultDict.

--- Dependencies/Utils.js
+++ Dependencies/Utils.js
@@ -1,23 +1,22 @@
 import { cloneDeep, zip, unzip, range, sortBy, sum } from 'lodash'
 
-export function argMax(arr){
-
-  // if(typeof arr !== 'Array')throw new Error('input must be of type array but given of type '+(typeof arr));
+/**
+ * given an array of numbers, this function returns the index of array member of greatest value.
+ * @param {number[]} arr 
+ * @returns {number} index
+ */
+function argMax(arr){
+  if(!Array.isArray(arr))throw new Error('input must be of type array but given '+arr);
   const maxVal = Math.max(...arr);
   if (maxVal === NaN)throw new Error('the entries in the given input array must be of type Number only.');
   return arr.indexOf(maxVal)
 }
 
 /**
- * 
- * @param {Number} index index
- * @param {Array} shape shape of the tensor
- * @param {Number} size total number of values in a tensor
- * 
- * @description given the index number of a tensor, this function coverts that index number to tensor's coordinates
- * 
+ * given the index of a tensor, this function coverts that index number to tensor's coordinates
+ * @param {number} index index
+ * @param {number[]} shape shape of the tensor
  * @example 
- * 
  * const tensor = 
     [
       [
@@ -36,11 +35,11 @@ export function argMax(arr){
 
     const coords = index2Coords(index, tensorShape, tensorSize );
     console.log(coords); // > [0, 1, 2]
+    @returns {number[]}  coords
  */
-export function index2Coords(index, shape, size){
+function index2Coords(index, shape){
 
-    size = size || shape.reduce((accumulator,currentValue)=> accumulator*currentValue, 1);
-
+    const size =shape.reduce((accumulator,currentValue)=> accumulator*currentValue, 1);
     const coords = [];
 
     let oldFac = size;
@@ -57,13 +56,24 @@ export function index2Coords(index, shape, size){
 }
 
 /**
- * 
- * @param {array} distribution 
- *@summary given an array of distribution, this function returns the random index by sampling from the given distribution.
+ * given an array of distribution, this function returns the random index by sampling from the given distribution.
+ * @param {number[]} distribution 
  * @example 
- * const a = [0.1, 0.3, 0.5]
+ * const dist = [0.1, 0.3, 0.5]
+ * let sampledIndex = sampleFromDistribution(dist);
+ * console.log('result: '+ sampledIndex);// > result: 2
+ * sampledIndex = sampleFromDistribution(dist);
+ * console.log('result: '+ sampledIndex);// > result: 1
+ * sampledIndex = sampleFromDistribution(dist);
+ * console.log('result: '+ sampledIndex);// > result: 2
+ * sampledIndex = sampleFromDistribution(dist);
+ * console.log('result: '+ sampledIndex);// > result: 2
+ * sampledIndex = sampleFromDistribution(dist);
+ * console.log('result: '+ sampledIndex);// > result: 0
+ * 
+ * @returns {number}
  */
-export function indexFromDistribution(distribution){
+function sampleFromDistribution(distribution){
   if (Math.max(distribution) === NaN)throw new Error('distribution must be an array whose entries must only be of type \'Number\'. ');
 
   const sumOfDist = sum(distribution);
@@ -79,26 +89,29 @@ export function indexFromDistribution(distribution){
   const [sortedDist, sortedIndexes ] = unzip(sortedDistAndIndexes);
   const cumDist = sortedDist.map((prob,i)=>sum(sortedDist.slice(0, i+1)));
   const indexOfNearestCumProbFromRandNum = ( cumDist.map((prob)=>{return 1*((prob-randNum) > 0)}) ).indexOf(1);
-  const sampledIndexFromDist = sortedIndexes.indexOf(indexOfNearestCumProbFromRandNum);
+  const sampledIndexFromDist = sortedIndexes[indexOfNearestCumProbFromRandNum];
   
   return sampledIndexFromDist;
 
 }
 
-export class DefaultDict{
+/**
+ * DefaultDict class inspired by numpy default_dict
+ */
+class DefaultDict{
 
   /**
    * 
-   * @param {object} defaultGetValue specify which value to return if the dictonary is queried with an unknown key.
+   * @param {Object} defaultGetValue specify which value to return if the dictonary is queried with an unknown key.
    */
   constructor(defaultGetValue = -1){
 
-    const Key = [];
-    const Value = [];
+    let Key = [];
+    let Value = [];
 
     /**
      * 
-     * @param {object} key 
+     * @param {Object} key 
      * @summary checks if the given key exist in our dict
      * @returns {boolean}
      */
@@ -111,12 +124,14 @@ export class DefaultDict{
 
     /**
      * 
-     * @param {object} key 
-     * @param {object} value 
+     * @param {Object} key 
+     * @param {Object} value 
      * @summary if the new key is provided then it will create a new entry in the dictonary with this 
      * new key-value pair. otherwise, update the existing value.
      */
     this.set = (key, value)=>{
+
+      if(typeof key === "function")throw new Error('DefaultDict does not support key of type \'function\' ')
 
       const stringifyKey = JSON.stringify(key);
 
@@ -134,8 +149,8 @@ export class DefaultDict{
 
     /**
      * 
-     * @param {object} key 
-     * @returns {object} returns a copy of the value associated with the given key
+     * @param {Object} key 
+     * @returns {Object} returns a copy of the value associated with the given key
      */
     this.get = (key)=>{
       
@@ -157,7 +172,7 @@ export class DefaultDict{
 
     /**
      * 
-     * @param {object} key 
+     * @param {Object} key 
      * @summary delete the specified key-value pairs from the dict. and shows a warning if the key doesn't exist
      * in the first place.
      */
@@ -190,20 +205,22 @@ export class DefaultDict{
         JSON.parse(k)
         ), Value);
     }
+
   }
 }
 
 
 /**
  * 
- * @param {string} policyFnName name of the policyFn to use
- * @param {object} env 
- * @param {object} params parameter for that particular policyFn
- * @summary this function contains a collection of different policyFns which you can use to specify the behaviour policy of your agent. 
+ * this function contains a collection of different policyFns which you can use to specify the behaviour policy of your agent. 
  * the full list of available poliyFns are:
  * ["epsilonGreedy", "UCB"]
+ * @param {string} policyFnName name of the policyFn to use
+ * @param {Object} env 
+ * @param {Object} params parameter for that particular policyFn
+ * @returns {Function}
  */
-export function policyFnFactory(policyFnName, env, params){
+function policyFnFactory(policyFnName, env, params){
   const nActions = env.nA;
 
   const policyFnNames = ['random', 'greedy', 'epsilonGreedy', 'UCB', ];
@@ -242,3 +259,4 @@ export function policyFnFactory(policyFnName, env, params){
   return policyFns[policyFnIndex];
 }
 
+export { argMax, index2Coords, sampleFromDistribution, DefaultDict, policyFnFactory };
