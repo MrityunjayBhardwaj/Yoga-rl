9ee5097292623656c46d5012ad33e3fa32933858
some <mrityunjaybhardwaj99@gmail.com>
2021-01-10 11:16:07 +0530

    ⚗ ♻️ 🐛 update app.js for policyIter and QLearning to support modified algorithm names.
    * tests/play with new features like save and load params and run the agent etc.

--- Algorithms/Dynamic-Programming/app.js
+++ Algorithms/Dynamic-Programming/app.js
@@ -1,17 +1,25 @@
 import { GridWorldEnv } from '../../Environments/gridWorld'
-import { policyIteration } from "./policyIteration";
+import { PolicyIterationAgent } from "./policyIteration"
 
+const gridSize = 4;
 
-const gridSize = 5;
-let env = new GridWorldEnv([gridSize, gridSize]);
+async function main(){
+    console.log("hello")
+    let env = new GridWorldEnv([gridSize, gridSize]);
+    let agent = new PolicyIterationAgent(
+        {
+            env: env,
+            discountFactor: 1.0,
+            theta: 0.00001
+        }
+    );
+    // train => save
+    // await agent.train(100);
+    // await agent.save();
+    console.log('it works')
 
-// init our policy Iteriation Model
-let model = new policyIteration(
-env,
-{   discountFactor: 1.0, 
-    theta: 0.00001 
-}, 
-);
-
-// run the policy iteration algorithm
-model.run([100, 100]);
+    // load params => run agent
+    await agent.load('./modelParams.json');
+    await agent.run(10);
+}
+main();
--- Algorithms/Temporal_Difference/QLearning/app.js
+++ Algorithms/Temporal_Difference/QLearning/app.js
@@ -1,10 +1,19 @@
 import { policyFnFactory } from "../../../Dependencies/Utils";
 import { BlackJackEnv } from "../../../Environments/blackJack";
+import { QLearningAgent } from './qLearning';
 
 // environment to test on.
 const env = new BlackJackEnv();
-
 // Testing QLearning
 const behaviorPolicy = policyFnFactory("epsilonGreedy", env, {epsilon: 0.1} );
-const model = new QLearning(env, {discountFactor: 1.0, learningRate: 0.5});
-model.run(1000, behaviorPolicy);
+const model = new QLearningAgent( {env, behaviorPolicy, discountFactor: 1.0, learningRate: 0.5});
+
+async function main(){
+    // await model.train(10000);
+    // await model.save();
+    const isSucessful = await model.load('./agentParams.json');
+    console.log('params: ', isSucessful, model.getParams());
+    await model.run();
+    window.q = model.getParams()
+}
+main();
